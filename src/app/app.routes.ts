1aeb01610dbb6099f7672972fc0f001a734b6bbb
CrVences <cristobal.avila@spaymentservices.com>
2024-09-30 16:09:31 -0600

    Se agrega estructura de carpetas atomic design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./components/templates/home/home.component').then(
+        (c) => c.HomeComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
