b9baed62e3e6e4c2c1a5995e4ff7fc48a66a77c3
CrVences <cristobal.avila@spaymentservices.com>
2024-09-30 16:58:50 -0600

    se agrega environments al proyecto

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apis: {
+    catalogs: 'http://localhost:3000',
+  },
+  origin: 'http://localhost:3000',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apis: {
+    catalogs: 'http://prod:3000',
+  },
+  origin: 'http://prod:3000',
+};
