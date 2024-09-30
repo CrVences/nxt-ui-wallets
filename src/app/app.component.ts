bf98b331595f352370e03267a0c0a8ce14ded038
CrVences <cristobal.avila@spaymentservices.com>
2024-09-30 16:22:45 -0600

    Se agrega libreria prime ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingComponent } from './components/pages/landing/landing.component';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingComponent],
+  imports: [RouterOutlet, LandingComponent, ToastModule],
+  providers: [MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/templates/home/home.component.ts
+++ src/app/components/templates/home/home.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { MessageService } from 'primeng/api';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ToastModule, ButtonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  constructor(private _messageService: MessageService) {}
 
+  public show() {
+    this._messageService.add({
+      severity: 'success',
+      summary: 'Success',
+      detail: 'Message Content',
+    });
+  }
 }
